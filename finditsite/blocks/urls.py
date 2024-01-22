4cc5aef84e86eaf7adf91d80a02e0293f6871a29
Valerii <v.bill2003@gmail.com>
2024-01-22 11:18:32 +0200

    rename correct names in url

--- finditsite/blocks/urls.py
+++ finditsite/blocks/urls.py
@@ -10,29 +10,29 @@ from . import views
 app_name = 'blocks'
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
-    path('main/', login_required(MianView.as_view()), name='loghome'),
+    path('main/', login_required(MianView.as_view()), name='log_home'),
 
     path('log-for-common/', CommonView.as_view(), name='prog1'),
-    path('common/', login_required(CommonLogView.as_view()), name='workprog1'),
+    path('common/', login_required(CommonLogView.as_view()), name='work_prog_1'),
 
     path('log-for-isolation/', IsolationView.as_view(), name='prog2'),
-    path('isolation/', login_required(IsolationLogView.as_view()), name='workprog2'),
+    path('isolation/', login_required(IsolationLogView.as_view()), name='work_prog_2'),
 
     path('log-for-detection/', DetectionView.as_view(), name='prog3'),
-    path('detection/', login_required(DetectionLogView.as_view()), name='workprog3'),
+    path('detection/', login_required(DetectionLogView.as_view()), name='work_prog_3'),
 
     path('login/', UserLoginView.as_view(), name='log'),
     path('registration/', UserRegistrationView.as_view(), name='reg'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('open_better_script/', views.open_better_script,
-         {'script_path': 'D:/work/project1/finditsite/1_better.py', 'redirect_path': 'workprog1',
+         {'script_path': 'D:/work/project1/finditsite/1_better.py', 'redirect_path': 'work_prog_1',
           'template_name': 'blocks/commonlog.html'}, name='open_better_script'),
     path('open_better_script2/', views.open_better_script,
-         {'script_path': 'D:/work/project1/finditsite/2_better.py', 'redirect_path': 'workprog2',
+         {'script_path': 'D:/work/project1/finditsite/2_better.py', 'redirect_path': 'work_prog_2',
           'template_name': 'blocks/isolationlog.html'}, name='open_better_script2'),
     path('open_better_script3/', views.open_better_script,
-         {'script_path': 'D:/work/project1/finditsite/3_better.py', 'redirect_path': 'workprog3',
+         {'script_path': 'D:/work/project1/finditsite/3_better.py', 'redirect_path': 'work_prog_3',
           'template_name': 'blocks/detectionlog.html'}, name='open_better_script3'),
 
     path('get_latest_photo/', views.get_latest_photo, {'category_id': '1'}, name='get_latest_photo'),
